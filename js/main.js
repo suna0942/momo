d9bf3aa76975634bd9fd9d14f11d4b05dfcb6f7e
suna0942 <suna0942@naver.com>
2022-02-19 23:09:38 +0900

    momo 시작

---
+++ js/main.js
@@ -0,0 +1,14 @@
+new Swiper ('header .mainVisual', {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+  navigation: {
+    prevEl: '.swiper-prev',
+    nextEl: '.swiper-next'
+  }
+});
