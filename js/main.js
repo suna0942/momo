7e65e2397b3f57263dc4ff0a387cbaf936ac1d64
suna0942 <suna0942@naver.com>
2022-03-13 02:23:55 +0900

    best menu start

--- js/main.js
+++ js/main.js
@@ -12,3 +12,20 @@ new Swiper ('.mainVisual', {
     nextEl: '.swiper-next'
   }
 });
+
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input');
+
+searchEl.addEventListener('click', function() {
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', function() {
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder', '통합검색');
+});
+
+searchInputEl.addEventListener('blur', function(){
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', '');
+})
